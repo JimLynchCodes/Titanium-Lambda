9647b37470fcd3893e150499b769f5cd024dab38
Jim Lynch <mrdotjim@gmail.com>
2018-06-26 10:50:14 -0400

    Create app.barebones-express.js

---
+++ app.barebones-express.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send({
+    "Output": "Hello World!"
+  });
+});
+
+app.post('/', function(req, res) {
+  res.send({
+    "Output": "Hello World!"
+  });
+});
+
+module.exports = app
